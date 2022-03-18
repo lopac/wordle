52500ada676ad8439fed3e84025adfb1b9d8008a
Antonio Lopac <lopac>
2022-03-18 17:24:07 +0100

    Implement theme mode button
    Squashed commit of the following:
    
    commit 3dcaffc3197845b0851828e6d630b9bb116466dd
    Author: Antonio Lopac <lopac>
    Date:   Fri Mar 18 00:44:10 2022 +0100
    
        Implement theme mode button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import ThemeModeBtn from './components/ThemeModeBtn';
+import { ThemeContext } from './contexts/ThemeContext';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline test">Hello wordle!</h1>;
+  const { isDarkMode } = React.useContext(ThemeContext);
+
+  return (
+    <div className={isDarkMode ? 'dark' : ''}>
+      <div className="px-8 pt-6 bg-white dark:bg-slate-900 h-screen">
+        <div className="grid grid-cols-3 gap-0">
+          <div />
+          <h2 className="text-center font-nova tracking-widest text-5xl dark:text-white">Wordle</h2>
+          <div className="text-right">
+            <ThemeModeBtn />
+          </div>
+        </div>
+        <hr className="mt-6" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nova: ['Nova Mono', 'monospace'],
+      },
+    },
   },
   variants: {},
   plugins: [],
+  darkMode: 'class',
 };
